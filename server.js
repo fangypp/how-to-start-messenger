08486888b316ee709b441f3fd2a5a872ca3ec47a
fangypp <fangypp@fangypps-MacBook.local>
2016-12-10 16:20:04 +0700

    fangbot1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const token = 'key'
+const token = 'EAAaG7VDrL3kBABQLWUHRIwe4NGTrp4GwrvZB5WTFYZAOYtdQLUh8bkrlxuyW7bxDMUBeIfTpxOdQYQt5W1YDJUXp9YscCDbw6zw6sNYyposksK1ptAngdcohrHeIbWtMoSdz4k19WBjLnOB0ioZBZAhHtg7CWlnGtUPqZBrN4PgZDZD'
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.send('test test')
 })
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '') {
+  if (req.query['hub.verify_token'] === 'passbot') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
