8091c9bddb71ba10a3008c40dd70e6cbca0dcce9
fangypp <fangypp@fangypps-MacBook.local>
2016-12-11 10:47:53 +0700

    wether key coutnstring recive

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/webhook/', function (req, res) {
               sendGenericMessage(sender)
               continue
             }
-      if (text.substring(0, 6) === 'Weather') {
+      if (text.substring(0, 7) === 'Weather') {
               var count =text.length
               sendTextMessage (sender, "อากาศตอนนี้หนาว")
               //sendTextMessage (sender, count)
