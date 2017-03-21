b605fd5344c76608c68c28ae6574dfa9940bed6f
fangypp <frangy.lolz@gmail.com>
2017-03-21 11:31:44 +0700

    try to say hello 5

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-sendTextMessage(sender, "Hello test");  
+//sendTextMessage(sender, "Hello test");  
 let messaging_events = req.body.entry[0].messaging
 for (let i = 0; i < messaging_events.length; i++) {
 let event = req.body.entry[0].messaging[i]
